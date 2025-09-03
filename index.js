6bbaf72a53394bd6f341fc9987434399b99d4b2d
Mujahid Hoshen <mujahidhoshen@Mujahids-MacBook-Air.local>
2025-09-03 19:32:07 +0300

    frist feature added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("conected");
+// life-line increase
+
+const clickButtons = document.querySelectorAll(".click-heart");
+for (let clickButton of clickButtons){
+    clickButton.addEventListener("click", function(){
+        const lifeLine = document.getElementById("life").innerText;
+        const lifeLineIncrease = Number(lifeLine)+1;
+        document.getElementById("life").innerText=lifeLineIncrease;
+        
+    })
+    
+}
